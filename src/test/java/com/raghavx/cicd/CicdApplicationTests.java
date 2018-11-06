00fe210cb0fbed3917d152570c99f8c85a89ba5d
Raghvendra <ragh@structure101.com>
2018-11-06 21:11:39 +0530

    initial setup

---
+++ src/test/java/com/raghavx/cicd/CicdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.raghavx.cicd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
